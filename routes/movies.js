aaf3dc9400e442a02d21e6c16cfedee98363f543
David Lewis <davidlewisp@gmail.com>
2019-03-07 12:58:23 -0800

    successfully add and test route to update a movie listing

--- routes/movies.js
+++ routes/movies.js
@@ -32,10 +32,27 @@ router.post('/add', (req, res) => {
   const newMovie = new Movie({
     name: req.body.name,
     description: req.body.description,
-    year: req.body.year,
+    year: req.body.year
   });
 
   newMovie.save().then(movie => res.json(movie));
+});
+
+// @route Post movies/update/:id
+// @desc Update specific movie
+router.post('/update/:id', (req, res) => {
+  const movieFields = {};
+    if (req.body.name) movieFields.name = req.body.name;
+    if (req.body.description) movieFields.description = req.body.description;
+    if (req.body.year) movieFields.year = req.body.year;
+
+  Movie.findOneAndUpdate(
+    { id: req.body.id },
+    { $set: movieFields },
+    { new: true }
+  )
+    .then(movie => res.json(movie))
+    .catch(err => res.status(404).json({cannotUdate: "Could not update movie"}));
 });
 
 
