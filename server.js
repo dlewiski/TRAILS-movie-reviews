cfd453c3bbd8ffdc73ba8ca5c61226efa87946df
David Lewis <davidlewisp@gmail.com>
2019-03-08 02:26:11 -0800

    update server file to prepare app for deployment to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const movies = require('./routes/movies');
 
@@ -8,7 +9,7 @@ const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -22,6 +23,16 @@ app.get('/', (req, res) => res.send("App route working!"));
 
 // Use movie routes
 app.use('/movies', movies);
+
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 4000;
 
