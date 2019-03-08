b8374f0f50be1733f3484d21f7c7a19e79a855eb
David Lewis <davidlewisp@gmail.com>
2019-03-08 11:01:54 -0800

    fixed type on server file, redeploy

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/movies', movies);
 // Server static assets if in production
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, 'client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
